02eeb12a718fd65de03eadf7a01195916cc5d408
VanOvermeirenWout <wout.van.overmeiren@student.howest.be>
2018-10-08 17:58:01 +0200

    nieuwe week oefeningen

---
+++ script/app.js
@@ -0,0 +1,20 @@
+function handleFloatingLabel() {
+    let input = document.querySelector('.js-name-input'),
+        label = document.querySelector('.js-floating-label');
+
+    input.addEventListener('blur', function() {
+        console.log(event);
+        if (input.value) {
+            label.classList.add('is-floating');
+        } else {
+            label.classList.remove('is-floating');
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log('Script loaded');
+    handleFloatingLabel();
+
+
+});
